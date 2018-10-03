efc897466a710af9b869d48b7dd99406c3f8cb07
root <root@gd.crownover>
2018-10-03 23:52:43 +0000

    got file

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv nginx 
+apt install -y python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv nginx 
 
 ln -s nginx/garage_door /etc/nginx/sites-enabled/garage_door
 
