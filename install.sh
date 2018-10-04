c05541d2541afccf1b8e96eb3e6f45c0b9cac2b9
root <root@garage_door.crownover>
2018-10-04 00:11:15 +0000

    yay

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+useradd gd
+chown -R gd:www-data /opt/garage_door
+chmod -R 775 /opt/garage_door
+
 apt install -y python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv nginx 
 
-ln -s nginx/garage_door /etc/nginx/sites-enabled/garage_door
+python3.6 -m venv venv
+source venv/bin/activate
+pip install wheel
+pip install flask uwsgi
+deactivate
 
-ln -s service/garage_door.service /etc/systemd/system/garage_door.service
+cp nginx/garage_door /etc/nginx/sites-enabled/garage_door
+cp service/garage_door.service /etc/systemd/system/garage_door.service
+systemctl daemon-reload
 
 ufw allow 420
 
